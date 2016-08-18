464ec5b0e74505432a56267c486edd21df0ce725
yamagh <hiroyuki.yamagami@gmail.com>
2016-08-19 00:15:05 +0900

    0時〜9時に実行するとフォルダの作成に失敗する問題修正

--- bkp.bat
+++ bkp.bat
@@ -1,7 +1,8 @@
 @echo off
 
 set bkp_dir=%UserProfile%\bkp
-set dest_dir=%bkp_dir%\%date:~0,4%-%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%-%time:~6,2%%time:~9,2%
+set time2=%time: =0%
+set dest_dir=%bkp_dir%\%date:~0,4%-%date:~5,2%%date:~8,2%-%time2:~0,2%%time2:~3,2%-%time2:~6,2%%time2:~9,2%
 
 if not exist %bkp_dir% (
   mkdir %bkp_dir%
