ea9632d59bd79954326c199375a940d1fa021231
yamagh <hiroyuki.yamagami@gmail.com>
2016-08-27 11:54:08 +0900

    Change current timestamp folder name format

--- backup.bat
+++ backup.bat
@@ -1,30 +1,41 @@
 @echo off
 
-set bkp_dir=%UserProfile%\bkp
-set time2=%time: =0%
-set dest_dir=%bkp_dir%\%date:~0,4%-%date:~5,2%%date:~8,2%-%time2:~0,2%%time2:~3,2%-%time2:~6,2%%time2:~9,2%
+:INITIALIZE
 
-if not exist %bkp_dir% (
-  mkdir %bkp_dir%
-  if "%ErrorLevel%" == "0" (
-    echo Created backup root folder
-  ) else (
-    echo Failed to create backup folder
+  set bkp_dir=%UserProfile%\bkp
+  set time2=%time: =0%
+  set dest_dir=%bkp_dir%\%date:/=-%-%time2::=%
+
+:CREATE_BACKUP_FOLDER
+
+  if not exist %bkp_dir% (
+    mkdir %bkp_dir%
+    if "%ErrorLevel%" == "0" (
+      echo Created backup root folder
+    ) else (
+      echo Failed to create backup folder
+    )
+    echo %bkp_dir%
   )
-  echo %bkp_dir%
-)
 
-echo BackupFolder: %dest_dir%
-mkdir %dest_dir%
+:CREATE_CURRENT_TIMESTAMP_FOLDER
 
-for %%a in (%*) do (
-  xcopy %%a %dest_dir% /D /S /R /Y /I /K /H
-  
-  if not "%ErrorLevel%" == "0" (
-    echo Failed...
-    pause
+  echo BackupFolder: %dest_dir%
+  echo.
+  mkdir %dest_dir%
+
+:BACKUP_ARGUMENTS
+
+  for %%a in (%*) do (
+    xcopy %%a %dest_dir% /D /S /R /Y /I /K /H
+    
+    if not "%ErrorLevel%" == "0" (
+      echo Failed...
+      pause
+    )
   )
-)
 
-timeout 3
+:FINISH
+
+  timeout 5
 
